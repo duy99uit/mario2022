c9207562d5d0ff3d8782af84fa2375e7cb721429
Doan The Duy <duy99.uit@gmail.com>
2022-12-12 21:01:24 +0700

    fix piranha func update

--- 05-SceneManager/PiranhaPlant.cpp
+++ 05-SceneManager/PiranhaPlant.cpp
@@ -18,53 +18,21 @@ PiranhaPlant::PiranhaPlant()
 
 void PiranhaPlant::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects) {
 
-	if (y <= limitY && vy < 0)
-	{
-		y = limitY;
-		SetState(PIRANHAPLANT_STATE_BITING);
-	}
-	if (y >= limitY + PIRANHAPLANT_BBOX_HEIGHT && vy > 0)
-	{
-		y = limitY + PIRANHAPLANT_BBOX_HEIGHT;
-		SetState(PIRANHAPLANT_STATE_INACTIVE);
-	}
-	if (GetTickCount64() - biting_start >= PIRANHAPLANT_BITING_TIME && biting_start != 0)
-	{
-		if (y == limitY)
-			vy = PIRANHAPLANT_DARTING_SPEED;
-		biting_start = 0;
-	}
 	CGameObject::Update(dt, coObjects);
 
-	y += vy * dt;
 	//x += vx * dt;
-	vector<LPCOLLISIONEVENT> coEvents;
-	vector<LPCOLLISIONEVENT> coEventsResult;
+	y += vy * dt;
 
-	coEvents.clear();
-	for (UINT i = 0; i < coEvents.size(); i++) delete coEvents[i];
-
+	// die by mario tail
 	CMario* mario = ((CPlayScene*)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
-	if (mario != NULL) {
-		float mLeft, mTop, mRight, mBottom;
-		float mWidth = mario->GetWidth();
-		mario->GetBoundingBox(mLeft, mTop, mRight, mBottom);
-		
-		if (isColliding(floor(mLeft), mTop, ceil(mRight), mBottom)) {
-			if (mario->GetLevel() != MARIO_LEVEL_SMALL)
-			{
-				mario->SetLevel(mario->GetLevel() - 1);
-			}
-			else
-			{
-				DebugOut(L" Mario DIE  \n");
-				/*mario->SetState(MARIO_STATE_DIE);*/
-			}
+	float mLeft, mTop, mRight, mBottom;
+	if (mario->GetLevel() == MARIO_LEVEL_TAIL) {
+		mario->tail->GetBoundingBox(mLeft, mTop, mRight, mBottom);
+
+		if (isColliding(floor(mLeft), mTop, ceil(mRight), mBottom) && mario->isTuring) {
+			DebugOut(L"PiranhaPlant die by mario tail \n");
+			//SetState();
 		}
-		if ((floor(mario->getX()) + (float)mWidth + PIRANHAPLANT_ACTIVE_RANGE <= x
-			|| ceil(mario->getX()) >= x + PIRANHAPLANT_BBOX_WIDTH + PIRANHAPLANT_ACTIVE_RANGE)
-			&& state == PIRANHAPLANT_STATE_INACTIVE && biting_start == 0)
-			SetState(PIRANHAPLANT_STATE_DARTING);
 	}
 }
 
